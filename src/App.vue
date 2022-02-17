02dfcc98c840fc77fa8b6f7d076c9d2d742c7b46
WojciechLipiński <lipinski.wo@gmail.com>
2022-02-17 13:43:46 +0100

    make it work first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div v-if="surveyFinished">
+      <div v-for="shoe in getShoesByRating" :key="shoe.id">
+        {{ shoe }}
+        <img :src="require(`@/assets/${shoe.name}.png`)" />
+      </div>
+    </div>
+    <div v-else>
+      <div>
+        <p>{{ questions[questionIndex].copy }}</p>
+        <div>
+          <button
+            v-for="answer in questions[questionIndex].answers"
+            :key="answer.id"
+            @click="handleAnswer(answer)"
+          >
+            {{ answer.copy }}
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from "../data.json";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
+  data: () => ({
+    questionIndex: 0,
+    surveyFinished: false,
+    ratings: {},
+    questions: data.questions,
+    shoes: data.shoes.reduce((acc, v) => {
+      acc[v.id] = v;
+      return acc;
+    }, {}),
+  }),
+  computed: {
+    getShoesByRating() {
+      const shoesNames = Object.entries(this.ratings)
+        .sort((a, b) => b[1] - a[1])
+        .map(([key, rating]) => ({ ...this.shoes[key], rating }));
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+      return shoesNames;
+    },
+  },
+  methods: {
+    handleAnswer({ ratingIncrease, nextQuestion }) {
+      this.updateRatings(ratingIncrease);
+
+      if (nextQuestion === "") {
+        this.surveyFinished = true;
+        return;
+      }
+
+      this.questionIndex = nextQuestion;
+    },
+    updateRatings(ratingIncrease) {
+      const entries = Object.entries(ratingIncrease);
+
+      this.ratings = entries.reduce((acc, [key, value]) => {
+        const oldRatingValue = acc[key] || 0;
+
+        acc[key] = oldRatingValue + value;
+
+        return acc;
+      }, this.ratings);
+    },
+  },
+};
+</script>
